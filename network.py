e21d9e85096cdfa456065883f49f5af7dc0cfcff
hak04-cs <humzak2@illinois.edu>
2023-11-16 01:34:25 -0600

    updated network mapping - needs further work on visualization but solved dup nodes implement keywords on webscraper test req timing for full database run look into further mapping

--- network.py
+++ network.py
@@ -3,63 +3,75 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import re
 import textwrap
-
-# Extract strings enclosed in single quotes using regular expression
+import scipy
 
 # Example list of sets
-csv_path = "C:\\Users\\hkdeb\\PycharmProjects\\ie5bot\\ieee.csv"
+csv_path = "C:\\Users\\hkdeb\\PycharmProjects\\ie5bot\\ieee_f300_raw.csv"
 
 df = pd.read_csv(csv_path)
 
-
-institution_sets_list = df['Affiliations']
+raw_inst_strs = df['Affiliations']
 institutions = []
-#print(institution_sets_list)
-#print(institution_sets_list[0].split("', '")[0].strip('{').strip('}'))
-for element in institution_sets_list:
+for element in raw_inst_strs:
     matches = re.findall(r"'(.*?)'", element)
 
     result_list = list(set(matches))
     institutions.append(result_list)
 
-#print(institutions)
-G = nx.Graph()
+connections_dict = {}
+instcheck = []
+# Iterate over sets
+for institution_set in institutions:
+    # Convert the set to a list for easier iteration
+    institutions_list = list(institution_set)
 
-for element in institutions:
-    #print(element)
-    G.add_nodes_from(element)
-    for i in range(0, len(element) - 1):
-        if len(element) > 1:
-            G.add_edge(element[i], element[i+1])
+    # Iterate over each institution in the set
+    for institution in institutions_list:
+        instcheck.append(institution)
+        # Check if the institution is already a key in the dictionary
+        if institution not in connections_dict:
+            # If not, add it with an empty list as the value
+            connections_dict[institution] = []
 
-#G.add_nodes_from([1,2,3,4,5])
-#G.add_edge(1,2)
-isolated_nodes = list(nx.isolates(G))
-G.remove_nodes_from(isolated_nodes)
-node_degrees = dict(G.degree())
-#print(node_degrees)
-# Determine the threshold for the top 10% of nodes
-print(max(node_degrees.values()))
-#threshold = int(0.8 * max(node_degrees.values()))
-threshold = 4
-# Keep only nodes with degree above the threshold
-top_nodes = [node for node, degree in node_degrees.items() if degree >= threshold]
-filtered_graph = G.subgraph(top_nodes)
+        # Add connections to other institutions in the same set
+        connections_dict[institution].extend(
+            other_inst for other_inst in institutions_list if other_inst != institution)
 
-# Set a larger figure size
+# Remove duplicates from the connection lists
+connections_dict = {key: list(set(value) - {key}) for key, value in connections_dict.items()}
+new_dict = {}
+num_connections_dict = {}
+for item in connections_dict.items():
+    if len(item[1]) >= 5:
+        new_dict[item[0]] = item[1]
+    num_connections_dict[item[0]] = len(item[1])
+G = nx.DiGraph()
 plt.figure(figsize=(15, 15))
 
-# Customize axis limits for a zoomed-out effect
+# Add nodes and edges to the graph
+for institution, related_institutions in new_dict.items():
+    G.add_node(institution)
+    G.add_edges_from([(institution, related_inst) for related_inst in related_institutions])
+
+pos = nx.spring_layout(G, k=1000000)
+labels = {node: '\n'.join(textwrap.wrap(node, width=15)) for node in G.nodes}
+colormap = []
+node_degrees = dict(G.degree())
+
+for node, degree in node_degrees.items():
+    colormap.append((1/(1+2*float(degree)), float(degree)*12/255, 0))
+
+
+# Draw nodes, edges, and labels
+nx.draw_networkx_nodes(G, pos, node_color=colormap, node_size=2000)
+nx.draw_networkx_edges(G, pos, edge_color="black", width=2)
+nx.draw_networkx_labels(G, pos, labels=labels,font_size=4, font_color="white", font_weight="bold")
+
+
 plt.axis('off')  # Turn off axis
 plt.xlim([-1.5, 1.5])
 plt.ylim([-1.5, 1.5])
-labels = {node: '\n'.join(textwrap.wrap(node, width=15)) for node in G.nodes}
-
-# Draw the graph
-pos = nx.spring_layout(G, k=5000)  # Adjust the 'k' parameter for node and edge spacing
-nx.draw_networkx_nodes(G, pos, node_color="skyblue", node_size=10000)
-nx.draw_networkx_edges(G, pos, edge_color="gray", width=2)
-nx.draw_networkx_labels(G, pos, labels=labels,font_size=8, font_color="black", font_weight="bold")
 
 # Show the plot
 plt.show()
+
