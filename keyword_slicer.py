e71b26d08afcfd63e34760f3b3767cb2e3b48e0d
roomtempfusion <humzakhan2026@u.northwestern.edu>
2023-12-19 17:06:47 -0600

    Updated keyword slicer and patent logic

--- keyword_slicer.py
+++ keyword_slicer.py
@@ -8,36 +8,39 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 csv_path = "C:\\Users\\hkdeb\\PycharmProjects\\ie5bot\\data_processed.csv"
 df = pd.read_csv(csv_path)
+df.fillna('', inplace=True)
 
 df['IEEE Terms'] = df['IEEE Terms'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
 df['Author Keywords'] = df['Author Keywords'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
 
-keywords = ['integrated circuit packaging', 'IC packaging', 'integrated packaging', "semiconductor device packaging"]
-
+# keywords = ['integrated circuit packaging', 'IC packaging', 'integrated packaging', "semiconductor device packaging"]
+#keywords = ['Memristor', 'Architecture', 'Thermal processing', 'Power management', 'Parallel processing', 'TPUs',
+ #           'Leading-edge node', 'Electronic design automation', 'tensor processing unit']
+keywords = ['gallium', 'Gallium Nitride', 'Gallium Oxide', 'Silicon Carbide', 'GaN', 'Ga2O3', 'SiC', 'power amplifiers',
+            'power amplifier architectures', 'wide band gap semiconductors', 'power electronics',
+            'power semiconductor devices', 'photonic band gap', 'wide band gap', 'power semiconductor']
+lower_keys = [key.lower() for key in keywords]
 new_df = pd.DataFrame(columns=df.columns)
-counter = -1
-for index, row in df.iterrows():
-    for key in keywords:
-        if key.upper() in str(row[0]).upper():
-            new_df.loc[len(new_df.index)] = row
-            continue
-        if key.upper() in str(row[5]).upper():
-            new_df.loc[len(new_df.index)] = row
-            continue
-    for element in row[-5]:
-        for key in keywords:
-            if key.upper() in element.upper():
-                new_df.loc[len(new_df.index)] = row
-                continue
 
-    for element in row[-6]:
-        for key in keywords:
-            if key.upper() in element.upper():
-                new_df.loc[len(new_df.index)] = row
-                continue
 
+def key_searcher(row):
+    for key in lower_keys:
+        if key in row['Document Title'].lower():
+            return True
+        # if key in row['Abstract'].lower():
+        #     return True
+    lower_ieee = [key.lower() for key in row['IEEE Terms']]
+    lower_author = [key.lower() for key in row['Author Keywords']]
+    if set(lower_keys).intersection(set(lower_ieee)):
+        return True
+    if set(lower_keys).intersection(set(lower_author)):
+        return True
+    return False
+
+
+mask = df.apply(key_searcher, axis=1)
+df = df[mask]
 
 df['IEEE Terms'] = df['IEEE Terms'].apply(lambda x: json.dumps(x) if isinstance(x, list) else x)
 df['Author Keywords'] = df['Author Keywords'].apply(lambda x: json.dumps(x) if isinstance(x, list) else x)
-
-new_df.drop_duplicates(subset=['Document Title']).to_csv('keyword_parsed.csv', index=False)
+df.to_csv('keyword_parsed_wbg_t.csv', index=False)
--- patentview.py
+++ patentview.py
@@ -7,20 +7,189 @@ import pandas as pd
 import json
 import sys
 api_url = 'https://api.patentsview.org/patents/query'
-
-abstract_search_term = 'memristor'
+country_dict = {
+    'AF': 'Islamic Republic of Afghanistan',
+    'AL': 'Republic of Albania',
+    'DZ': 'People\'s Democratic Republic of Algeria',
+    'AD': 'Principality of Andorra',
+    'AO': 'Republic of Angola',
+    'AR': 'Argentine Republic',
+    'AM': 'Republic of Armenia',
+    'AU': 'Commonwealth of Australia',
+    'AT': 'Republic of Austria',
+    'AZ': 'Republic of Azerbaijan',
+    'BH': 'Kingdom of Bahrain',
+    'BD': 'People\'s Republic of Bangladesh',
+    'BY': 'Republic of Belarus',
+    'BE': 'Kingdom of Belgium',
+    'BJ': 'Republic of Benin',
+    'BT': 'Kingdom of Bhutan',
+    'BO': 'Plurinational State of Bolivia',
+    'BA': 'Republic of Bosnia and Herzegovina',
+    'BW': 'Republic of Botswana',
+    'BR': 'Federative Republic of Brazil',
+    'BG': 'Republic of Bulgaria',
+    'BF': 'Burkina Faso',
+    'BI': 'Republic of Burundi',
+    'KH': 'Kingdom of Cambodia',
+    'CM': 'Republic of Cameroon',
+    'CA': 'Canada',
+    'CV': 'Republic of Cabo Verde',
+    'CF': 'Central African Republic',
+    'TD': 'Republic of Chad',
+    'CL': 'Republic of Chile',
+    'CN': 'People\'s Republic of China',
+    'CO': 'Republic of Colombia',
+    'KM': 'Union of the Comoros',
+    'CR': 'Republic of Costa Rica',
+    'HR': 'Republic of Croatia',
+    'CU': 'Republic of Cuba',
+    'CY': 'Republic of Cyprus',
+    'CZ': 'Czech Republic',
+    'DK': 'Kingdom of Denmark',
+    'DJ': 'Republic of Djibouti',
+    'DO': 'Dominican Republic',
+    'EC': 'Republic of Ecuador',
+    'EG': 'Arab Republic of Egypt',
+    'SV': 'Republic of El Salvador',
+    'GQ': 'Republic of Equatorial Guinea',
+    'ER': 'State of Eritrea',
+    'EE': 'Republic of Estonia',
+    'ET': 'Federal Democratic Republic of Ethiopia',
+    'FJ': 'Republic of Fiji',
+    'FI': 'Republic of Finland',
+    'FR': 'French Republic',
+    'GA': 'Gabonese Republic',
+    'GM': 'Republic of The Gambia',
+    'GE': 'Georgia',
+    'DE': 'Federal Republic of Germany',
+    'GH': 'Republic of Ghana',
+    'GR': 'Hellenic Republic',
+    'GT': 'Republic of Guatemala',
+    'GN': 'Republic of Guinea',
+    'GW': 'Republic of Guinea-Bissau',
+    'GY': 'Co-operative Republic of Guyana',
+    'HT': 'Republic of Haiti',
+    'HN': 'Republic of Honduras',
+    'HU': 'Hungary',
+    'IS': 'Republic of Iceland',
+    'IN': 'Republic of India',
+    'ID': 'Republic of Indonesia',
+    'IR': 'Islamic Republic of Iran',
+    'IQ': 'Republic of Iraq',
+    'IE': 'Ireland',
+    'IL': 'State of Israel',
+    'IT': 'Italian Republic',
+    'JM': 'Jamaica',
+    'JP': 'Japan',
+    'JO': 'Hashemite Kingdom of Jordan',
+    'KZ': 'Republic of Kazakhstan',
+    'KE': 'Republic of Kenya',
+    'KR': 'Republic of Korea',
+    'KW': 'State of Kuwait',
+    'KG': 'Kyrgyz Republic',
+    'LA': 'Lao People\'s Democratic Republic',
+    'LV': 'Republic of Latvia',
+    'LB': 'Lebanese Republic',
+    'LS': 'Kingdom of Lesotho',
+    'LR': 'Republic of Liberia',
+    'LY': 'State of Libya',
+    'LI': 'Principality of Liechtenstein',
+    'LT': 'Republic of Lithuania',
+    'LU': 'Grand Duchy of Luxembourg',
+    'MK': 'Republic of North Macedonia',
+    'MG': 'Republic of Madagascar',
+    'MW': 'Republic of Malawi',
+    'MY': 'Malaysia',
+    'ML': 'Republic of Mali',
+    'MT': 'Republic of Malta',
+    'MR': 'Islamic Republic of Mauritania',
+    'MU': 'Republic of Mauritius',
+    'MX': 'United Mexican States',
+    'MD': 'Republic of Moldova',
+    'MC': 'Principality of Monaco',
+    'MN': 'Mongolia',
+    'ME': 'Montenegro',
+    'MA': 'Kingdom of Morocco',
+    'MZ': 'Republic of Mozambique',
+    'MM': 'Republic of the Union of Myanmar',
+    'NA': 'Republic of Namibia',
+    'NP': 'Federal Democratic Republic of Nepal',
+    'NL': 'Kingdom of the Netherlands',
+    'NZ': 'New Zealand',
+    'NI': 'Republic of Nicaragua',
+    'NE': 'Republic of Niger',
+    'NG': 'Federal Republic of Nigeria',
+    'NO': 'Kingdom of Norway',
+    'OM': 'Sultanate of Oman',
+    'PK': 'Islamic Republic of Pakistan',
+    'PS': 'State of Palestine',
+    'PA': 'Republic of Panama',
+    'PY': 'Republic of Paraguay',
+    'PE': 'Republic of Peru',
+    'PH': 'Republic of the Philippines',
+    'PL': 'Republic of Poland',
+    'PT': 'Portuguese Republic',
+    'QA': 'State of Qatar',
+    'RO': 'Romania',
+    'RU': 'Russian Federation',
+    'RW': 'Republic of Rwanda',
+    'SA': 'Kingdom of Saudi Arabia',
+    'SN': 'Republic of Senegal',
+    'RS': 'Republic of Serbia',
+    'SC': 'Republic of Seychelles',
+    'SL': 'Republic of Sierra Leone',
+    'SG': 'Republic of Singapore',
+    'SK': 'Slovak Republic',
+    'SI': 'Republic of Slovenia',
+    'SO': 'Federal Republic of Somalia',
+    'ZA': 'Republic of South Africa',
+    'ES': 'Kingdom of Spain',
+    'LK': 'Democratic Socialist Republic of Sri Lanka',
+    'SD': 'Republic of the Sudan',
+    'SR': 'Republic of Suriname',
+    'SZ': 'Kingdom of Eswatini',
+    'SE': 'Kingdom of Sweden',
+    'CH': 'Swiss Confederation',
+    'SY': 'Syrian Arab Republic',
+    'TW': 'Taiwan',
+    'TJ': 'Republic of Tajikistan',
+    'TZ': 'United Republic of Tanzania',
+    'TH': 'Kingdom of Thailand',
+    'TL': 'Democratic Republic of Timor-Leste',
+    'TG': 'Togolese Republic',
+    'TT': 'Republic of Trinidad and Tobago',
+    'TN': 'Republic of Tunisia',
+    'TR': 'Republic of Turkey',
+    'TM': 'Turkmenistan',
+    'UG': 'Republic of Uganda',
+    'UA': 'Ukraine',
+    'AE': 'United Arab Emirates',
+    'GB': 'United Kingdom of Great Britain and Northern Ireland',
+    'US': 'United States of America',
+    'UY': 'Oriental Republic of Uruguay',
+    'UZ': 'Republic of Uzbekistan',
+    'VE': 'Bolivarian Republic of Venezuela',
+    'VN': 'Socialist Republic of Vietnam',
+    'YE': 'Republic of Yemen',
+    'ZM': 'Republic of Zambia',
+    'ZW': 'Republic of Zimbabwe',
+}
+abstract_search_term = '"photonic band gap"'
 # add with '_and' to narrow down cross-domain keywords: {'_text_any': {'patent_title/abstract': 'semiconductor'}}
-query = {'q': {'_or':
+query = {'q': {'_and': [{'_or':
                    [{'_text_any': {'patent_abstract': f'{abstract_search_term}'}},
                     {'_text_any': {'patent_title': f'{abstract_search_term}'}}
-                    ]},
+                    ]}, {'_or':
+                   [{'_text_any': {'patent_abstract': 'semiconductor'}},
+                    {'_text_any': {'patent_title': 'semiconductor'}}
+                    ]}]},
          'f': ['patent_title', 'ipc_section', 'ipc_class','ipc_subclass', 'inventor_country','inventor_latitude',
                'inventor_longitude',
                'patent_abstract', 'patent_number'],
          'o': {'per_page': 10000}}
 
 response = requests.post(api_url, json=query)
-
 df = pd.DataFrame(response.json()['patents'])
 
 # If no results, end program
@@ -93,8 +262,20 @@ def extract_coords(row):
     return coords_list
 
 
+def official_name(input_str):
+
+    # Convert input string to uppercase for case-insensitive matching
+    input_str_upper = input_str.upper()
+
+    for abbreviation, full_name in country_dict.items():
+        if input_str_upper == abbreviation.upper():
+            return full_name
+    return input_str
+
+
 # Apply the function to each row and create a new column 'inventor_countries'
 df['inventor_countries'] = df.apply(lambda row: list(set(extract_countries(row))), axis=1)
+df['inventor_countries'] = df['inventor_countries'].apply(lambda x: [official_name(country) for country in x])
 df['coords'] = df.apply(lambda row: extract_coords(row), axis=1)
 
 # Preserve list structure
@@ -106,7 +287,7 @@ for name in df.columns.tolist():
     if 'Inventor' in name:
         #print(name)
         df.drop(name, axis=1, inplace=True)
-abstract_search_term_safe = abstract_search_term.replace(' ', '_')
+abstract_search_term_safe = abstract_search_term.replace(' ', '_').replace('"', '')
 #print(abstract_search_term_safe)
 df.to_csv(f'patents_data_{abstract_search_term_safe}.csv', index=False)
 print(df)
